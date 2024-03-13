5ad5428a6f60d7fde2254358213c4c676d7ae374
AurelienPREVOST <aurelienprevost77@gmail.com>
2024-03-13 23:37:57 +0100

    first commit

--- public/js/classes/Projectile.js
+++ public/js/classes/Projectile.js
@@ -1,22 +1,24 @@
 class Projectile {
-  constructor(x, y, radius, color, velocity) {
-    this.x = x
-    this.y = y
-    this.radius = radius
-    this.color = color
-    this.velocity = velocity
+  constructor({ x, y, radius, color = "white", velocity }) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
   }
 
   draw() {
-    c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
-    c.fill()
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
   }
 
   update() {
-    this.draw()
-    this.x = this.x + this.velocity.x
-    this.y = this.y + this.velocity.y
+    this.draw();
+    console.log('this.x', this.x);
+    console.log('this.velocity.x', this.velocity.x);
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
   }
 }
